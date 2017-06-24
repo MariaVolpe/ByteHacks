6d303db2b9132968790e4c1dc61546518f3c7297
Maria Volpe <mariaCHVolpe@gmail.com>
2017-06-24 14:49:22 -0400

    Update Habits display

--- ByteApp/HabitsList.swift
+++ ByteApp/HabitsList.swift
@@ -31,25 +31,24 @@ class HabitsList: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return habits.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "habitCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        // Fetches the appropriate category for the data source layout.
+        let aHabit = habits[indexPath.row]
+        
+        cell.textLabel?.text = aHabit.name
         return cell
     }
-    */
-
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
