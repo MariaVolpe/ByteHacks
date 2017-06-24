be2b913de6cd250c5f4630a964ae57f6c2018320
Truly <trulyj.sof@gmail.com>
2017-06-24 13:09:42 -0400

    Create files with default methods for classes

--- ByteApp/CategoriesList.swift
+++ ByteApp/CategoriesList.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 class CategoriesList: UITableViewController {
-    
+
     //MARK: Properties
     
     var categories = [String]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadDefaultCategories()
@@ -24,10 +24,78 @@ class CategoriesList: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
     
     private func loadDefaultCategories() {
-        var cat1 = "To-Do List"
-        var cat2 = "Resources"
+        let cat1 = "To-Do List"
+        let cat2 = "Resources"
         var cat3 = "Meals"
         var cat4 = "Medication"
         var cat5 = "Chores"
@@ -37,7 +105,13 @@ class CategoriesList: UITableViewController {
         categories += [cat1, cat2, cat3, cat4, cat5, cat6, cat7]
         
     }
-
+    
+    
+    
+    
 
 }
+
+
+
 
