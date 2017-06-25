5718b2fc34ac0b962b706c3189da8865cb065b68
Truly <trulyj.sof@gmail.com>
2017-06-24 23:36:06 -0400

    Implement deletion of list items

--- ByteApp/CategoriesListViewController.swift
+++ ByteApp/CategoriesListViewController.swift
@@ -13,11 +13,11 @@ class CategoriesListViewController: UITableViewController {
     //MARK: Properties
     
     var categories = [Cat]()
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadDefaultCategories()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,25 +46,26 @@ class CategoriesListViewController: UITableViewController {
         return cell
     }
 
-    /*
+
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
+
+
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            categories.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
